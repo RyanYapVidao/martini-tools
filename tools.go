7c8f7a67e8d9c15a917ae9cee0c6b03549eb59c4
RyanYapVidao <ryan@vidao.co>
2014-11-03 15:24:13 -0800

    WIP

--- tools.go
+++ tools.go
@@ -6,13 +6,54 @@ import (
   "crypto/sha256"
   "fmt"
   "strconv"
+  "appengine/datastore"
+  "appengine"
+  "github.com/gorilla/securecookie"
+  "time"
   )
 
-type JsonReply struct {
-  Status string
-  Uid    string
-  Sid    string
+var key1 = []byte("5916569511133184")
+var key2 = []byte("4776259720577024")
+var CookieHandler = securecookie.New(key1, key2)
+
+type jsonreply JsonReply
+type loggedinusers Loggedinusers
+
+///////////////////////////////////
+
+func OpenidKey(c appengine.Context, openid string) *datastore.Key {
+  return datastore.NewKey(c, "openiduid", openid, 0, nil)
 }
+
+func SignUpLockKey(c appengine.Context, openid string) *datastore.Key {
+  return datastore.NewKey(c, "SignUpLock", openid, 0, nil)
+}
+
+func UsersKey(c appengine.Context) *datastore.Key {
+  return datastore.NewKey(c, "User", "", 0, nil)
+}
+
+func FillUsersKey(c appengine.Context, Userid int64) *datastore.Key {
+	return datastore.NewKey(c, "User", "", Userid, nil)
+}
+
+
+func LoginKey(c appengine.Context, Sessionid int64) *datastore.Key {
+	return datastore.NewKey(c, "login", "", Sessionid, nil)
+}
+
+func CreateloginKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "login", "", 0, nil)
+}
+
+func ProfileKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Profile", "", 0, nil)
+}
+
+func UnvalidatedKey(c appengine.Context, Userid int64) *datastore.Key {
+	return datastore.NewKey(c, "unvaildatedusers", "", Userid, nil)
+}
+///////////////////////////////////
 
 func Hash256(input string) (output string){
   hash := sha256.New()
@@ -47,4 +88,47 @@ func SendJson(w *http.ResponseWriter, r *http.Request, message string, uid strin
   (*w).Header().Add("X-Requested-With", "XMLHttpRequest")
   (*w).Write(js)
   return
+}
+
+func SetSession(userid int64, w *http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	g := &loggedinusers{
+		SID: 0,
+	}
+
+	key := CreateloginKey(c)
+	keyPut, err := datastore.Put(c, key, g)
+	if err != nil {
+    http.Error((*w), err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	value := map[string]string{
+		"userid":    strconv.FormatInt(userid, 10),
+		"sessionid": strconv.FormatInt(keyPut.IntID(), 10),
+	}
+	if encoded, err := CookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+			MaxAge: 3600,
+			Domain: ".auth-test-selva.appspot.com",
+
+
+		}
+    http.SetCookie((*w), cookie)
+
+		loginUser:=loggedinusers{
+			UID: userid,
+			SID : keyPut.IntID(),
+			Extime : time.Now().Unix() + 3600,
+		}
+		if _, errPut := datastore.Put(c, LoginKey(c, loginUser.SID), &loginUser); errPut != nil {
+      fmt.Fprint((*w), errPut)
+		}
+	}
+	//sendJson(&w, r, "User Logged In", "0", strconv.FormatInt(keyPut.IntID(), 10))
 }
